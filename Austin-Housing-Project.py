0aabc716b55fedb3c2c15c0bd56a90bf25d2cf1c
Abraham Olakunle Ajibade <abrahamjibbs1703@gmail.com>
2024-03-31 15:41:09 -0400

    Initial push of the notebook files to github repository

---
+++ Austin-Housing-Project.py
@@ -0,0 +1,12 @@
+# Austin Housing Project (Exploratory Data Analysis and Predictive Modelling)
+
+# Importing Necessary Libraries
+
+# Libraries for Data Analysis and Visualization
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Libraries for Machine Learning 
+

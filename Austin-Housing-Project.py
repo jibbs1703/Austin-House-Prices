4434ba05921fe9a2d2c4d9a92fa82129ff7d265e
Abraham Olakunle Ajibade <abrahamjibbs1703@gmail.com>
2024-04-07 21:16:00 -0400

    Made changes to code script

--- Austin-Housing-Project.py
+++ Austin-Housing-Project.py
@@ -1,12 +1,13 @@
 # Austin Housing Project (Exploratory Data Analysis and Predictive Modelling)
-
-# Importing Necessary Libraries
-
+# IMPORT NECESSARY LIBRARIES
 # Libraries for Data Analysis and Visualization
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import plotly.express as px
 import matplotlib.pyplot as plt
-
+from uszipcode import SearchEngine
+import folium
+from branca.colormap import linear, LinearColormap
 # Libraries for Machine Learning 
 
